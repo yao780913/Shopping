8937a8c011790c5a7b6e1a3b07adc1279405d368
bill.wang <bill.wang@edgentauems.com>
2024-08-21 13:52:51 +0800

    [init] books and carts

---
+++ Shopping.Api/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping.Api.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> ListBooks ()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet("{bookId}")]
+    public async Task<IActionResult> GetBookById (string bookId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("Search")]
+    public async Task<IActionResult> SearchBooks ([FromBody] SearchBooksRequest request)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpGet("/authors")]
+    public async Task<IActionResult> GetAuthors ([FromQuery] string authorId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    
+    
+    
+    
+}
+
+public class SearchBooksRequest { }
---
+++ Shopping.Api/Controllers/CartsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CartsController:ControllerBase
+{
+    [HttpGet("{cartId}")]
+    public Task<IActionResult> GetCart(string cartId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost("{cartId}/addItem")]
+    public Task<IActionResult> AddItemToCart(string cartId, [FromBody] AddItemToCartRequest request)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpDelete("{cartId}")]
+    public Task<IActionResult> ClearCart(string cartId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpDelete("{cartId}/items/{cartItemId}")]
+    public Task<IActionResult> RemoveItemFromCart(string cartId, string cartItemId)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class AddItemToCartRequest { }
